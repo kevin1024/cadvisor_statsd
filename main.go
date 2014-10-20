164fded19de8fcaea9f6541556ef5f3f8c8c98e2
Kevin McCarthy <me@kevinmccarthy.org>
2014-10-19 19:01:34 -1000

    lets use the official client

--- main.go
+++ main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
-	"github.com/kevin1024/cadvisor_statsd/cadvisor"
+	//"strconv"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/google/cadvisor/info"
+	"github.com/kevin1024/cadvisor/client"
+	//"github.com/cactus/go-statsd-client/statsd"
 	"time"
 )
 
+func ContainerStatsToStatsDStrings(cs *info.ContainerStats) []string {
+	fmt.Printf("---> %v\n", cs)
+	return []string{
+		//cs.Cpu.Usage.Total,
+		"whatever",
+		"stats",
+	}
+}
 
 func main() {
 	for {
-		output, _ := cadvisor.GetAllSubcontainers()
-		fmt.Printf("%v", output)
-		time.Sleep(5*time.Second)
+		client, _ := client.NewClient("http://192.168.59.103:8080/")
+		//mInfo, _ := client.MachineInfo()
+		request := info.ContainerInfoRequest{2}
+		sInfo, _ := client.ContainerInfo("/docker", &request)
+		for _, containerStats := range sInfo.Stats {
+		        spew.Dump(containerStats)
+			spew.Dump(ContainerStatsToStatsDStrings(containerStats))
+		}
+		time.Sleep(5 * time.Second)
 	}
 }
